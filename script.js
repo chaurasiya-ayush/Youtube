2ee7b03b64450187c37e1da0726611cc1159b4f5
Ayush-Chaurasiya <ayushchaurasiya9532951470@gmail.com>
2024-12-25 14:08:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+addEventListener("click",() =>{
+    document.getElementById("butt").style.backgroundColor = "red";
+    document.getElementById("butt").style.color = "white";
+    document.getElementById("butt").style.fontSize = "1rem";
+    document.getElementById("butt").innerHTML="Suscribed";
+   
+})
